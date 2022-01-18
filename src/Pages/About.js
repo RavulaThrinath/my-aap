37ddd6aec752b094fb0505ab54eea1adff5946f6
Ravula Thrinath <you@example.com>
2022-01-19 02:08:27 +0530

    About

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -18,17 +18,15 @@ function About() {
       >
         <h1 className="about-text">
           <h1 className="about-title">About US</h1>
-          <p className="tessst">
-            Intially we founded the company in 2021 june. We all have faced
-            tough situation during the lockdown. Covid-19 taught us don't rely
+          <p className="tessst">We founded this company in June, 2021. We all have faced very
+            tough situation during the lockdown. Covid taught us don't rely
             on single source of income. so we decided to think how to earn money
-            by multiple hustles and then decided to invest in the stock market. We
-            thought investing in the stock market was bit risk. Then finally we decided
-            to invest with lower amount. Intially we didn't get profits. Then we
+            by multiple hustles and then decided to invest in stock market. We
+            thought investing in the stock market was bit risk. Intially we didn't get profits. Then we
             took suggestions from my uncle who has been investing in stocks from past
             20 years. He helped us to get nice profits, With the help of him we
-            founded the company. We started to recommend some stocks to my
-            friends which gave a huge profits to them. Now currently we have
+            founded the company. We started to recommend stocks to my
+            friends and colleagues which gave a huge profits to them. Now currently we have
             more than 10 active clients.
           </p>
         </h1>
