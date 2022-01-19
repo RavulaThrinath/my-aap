bb4ae17cef4953e65a0cea61fdd487fdc996c0d6
Ravula Thrinath <you@example.com>
2022-01-19 16:33:25 +0530

    form responsive

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import RightImage from "../Assets/Right Image.png";
 import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
-import PropTypes from 'prop-types';
-import { styled } from '@mui/material/styles';
+import PropTypes from "prop-types";
+import { styled } from "@mui/material/styles";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
-import IconButton from '@mui/material/IconButton';
-import CloseIcon from '@mui/icons-material/Close';
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
 import Form from "../Components/Form";
 import Testimonials from "../Pages/Testimonials";
 import Quotes from "../Components/Quotes";
@@ -38,7 +38,7 @@ const BootstrapDialogTitle = (props) => {
           aria-label="close"
           onClick={onClose}
           sx={{
-            position: 'absolute',
+            position: "absolute",
             right: 8,
             top: 8,
             color: (theme) => theme.palette.grey[500],
@@ -57,7 +57,6 @@ BootstrapDialogTitle.propTypes = {
 };
 
 const Home = () => {
-
   // Down Arrows
   const [arrow, setArrow] = useState(true);
   const arrowVisible = () => {
@@ -83,7 +82,7 @@ const Home = () => {
   return (
     <div className="landing">
       <div className="Home">
-        <div className="home-content" >
+        <div className="home-content">
           <div className="left">
             <div className="Main-title1">Join the Revolutionary</div>
             <div className="Main-title2">NSP Stacks Solutions</div>
@@ -100,19 +99,13 @@ const Home = () => {
             >
               <BootstrapDialogTitle
                 id="customized-dialog-title"
-                onClose={handleClose} style={{color:"#dddddd", fontFamily: "Lato" , fontSize:"20px"}}
+                onClose={handleClose}
               >
-                Interested in becoming a client?
+                Interested in Becoming a client ? Enter your details and We will contact you.
               </BootstrapDialogTitle>
               <DialogContent>
-              <DialogContentText
-                  className="Dialog-des"
-                  style={{ color:"#dddddd", fontFamily: "Lato", fontSize:"17px", margin: " 0 0 30px 0" }}
-                >
-                  Dont hesitate to contact us. We are available <br /> 24/7 and
-                  look forward to hearing from you.
-                </DialogContentText>
-                <Form/>
+                <DialogContentText></DialogContentText>
+                <Form />
               </DialogContent>
             </BootstrapDialog>
           </div>
@@ -135,32 +128,3 @@ const Home = () => {
 };
 
 export default Home;
-
-// <Dialog
-// className="Dialog-box"
-// style={{ color: "#EEEEEE" }}
-// open={open}
-// onClose={handleClose}
-// >
-// <DialogTitle style={{ fontFamily: "Poppins", fontWeight: "600" }}>
-//   Interested in becoming a Client?
-// </DialogTitle>
-// <DialogContent>
-//   <DialogContentText
-//     className="Dialog-des"
-//     style={{ fontFamily: "Lato", margin: " 0 0 30px 0" }}
-//   >
-//     Dont hesitate to contact us. We're available 24/7 <br /> and
-//     look forward to hearing from you.
-//   </DialogContentText>
-//   <Form />
-// </DialogContent>
-// <DialogActions>
-//   <button className="cancel-btn" onClick={handleClose}>
-//     Cancel
-//   </button>
-//   <button className="submit-btn" onClick={handleClose}>
-//     Submit
-//   </button>
-// </DialogActions>
-// </Dialog>
