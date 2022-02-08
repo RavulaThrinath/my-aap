d7cefa7e6a4dd99d2a4bda51358f9c2a0cdd99b5
Ravula Thrinath <you@example.com>
2022-02-08 12:20:34 +0530

    Profession Change

--- src/Data.js
+++ src/Data.js
@@ -4,7 +4,7 @@ export  const Reviews = [
     review:
       "Since joining the NSP Stacks solutions and following their advice I have seen some great returns. Thank you NSP Stacks Solutions for teaching me so much and for the gains I have through long term investing.",
     name: "Mithun",
-    profession: "Data Scientist",
+    profession: " ",
     imageId: "01",
   },
   {
