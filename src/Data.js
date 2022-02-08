e0b1b01e727be456b1e9e955776849599a5a2deb
Ravula Thrinath <you@example.com>
2022-02-08 19:06:47 +0530

    names

--- src/Data.js
+++ src/Data.js
@@ -15,21 +15,21 @@ export const Reviews = [
     id: 1,
     review:
       "Great practical advice for long-term investors that can help you beat the market without losing your sanity. on the whole I am very happy with their recommendations. A happy customer since 1 year.",
-    name: "Srikanth",
-    profession: "Software Engineer",
+    name: "Harsha",
+    profession: " ",
   },
   {
     id: 2,
     review:
       "One of the best in the advisory space. I Have an overall good experience of their recommendations. I would strongly recommend NSP Stacks solutions to anyone wanting to achieve better financial outcomes.",
-    name: "Harish Pavan",
-    profession: "Software Engineer",
+    name: "Monish",
+    profession: "Student",
   },
   {
     id: 3,
     review:
       "First year with the NSP Stacks solutions. Some stocks are up, some are down. But I learned how to think about investing and that was well worth the price of admission. Amazing results in financial growth.",
-    name: "Sai Kiran",
-    profession: "Doctor",
+    name: "Suresh",
+    profession: "Business",
   },
 ];
